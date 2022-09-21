68e712aa10e0b56cab80705746b5f2563be6da04
ThomasF85 <thomas.foeldi@gmail.com>
2022-09-21 13:45:08 +0200

    add solutions

--- src/components/ActiveToggle/ActiveToggle.js
+++ src/components/ActiveToggle/ActiveToggle.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./ActiveToggle.css";
 
 function ActiveToggle() {
+  const [active, setActive] = useState(false);
+
   return (
     <main>
-      <div className="box" />
-      <button type="button">Activate</button>
+      <div className={`box${active ? " box--active" : ""}`} />
+      <button
+        onClick={() => setActive((previousActive) => !previousActive)}
+        type="button"
+      >
+        {active ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
